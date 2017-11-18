f57298f2804b90213ce6190f5fe7d742afde841e
Inequality Sign <inequality@InequalitydeMacBook.local>
2017-11-19 00:36:41 +0800

    Initial commit

---
+++ src/main/java/com/yyx/mapr/test/WordCountMapper.java
@@ -0,0 +1,35 @@
+package com.yyx.mapr.test;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * 继承map类
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    /**
+     * map阶段的业务逻辑
+     * @param key
+     * @param value
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        IntWritable one = new IntWritable(1);
+        //获取每行的内容
+        String line = value.toString();
+        //切分单词
+        String[] words = line.split(" ");
+        //将单词输出为<word 1>
+        for (String word : words) {
+            context.write(new Text(word), one);
+        }
+    }
+}
